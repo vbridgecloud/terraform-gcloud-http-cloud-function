03701cc228f3ad20702efdb676f46336062b0bea
Alex De Smet <alexdesmet12@gmail.com>
2020-04-02 14:21:12 +0200

    Update outputs.tf
    added function_name output

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "zip_name" {
   description = "Name of the .zip file that stored on GCS"
 }
 
+output "function_name" {
+  value       = "${google_cloudfunctions_function.function.name}"
+  description = "Name of the cloud function"
+}
+
 output "environment_variables" {
   value       = "${google_cloudfunctions_function.function.environment_variables}"
   description = "Environment Variables used in the Cloud Function"
